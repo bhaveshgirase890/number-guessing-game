1b58b71661e85b05476c81577d95ccf4476d3686
bhaveshgirase890 <bhaveshgirase890@gmail.com>
2026-04-08 15:30:05 +0530

    feat: update database dump with correct schema
    Added a new 'games' table to track user games and modified 'games_played' column to allow NULL values.

--- number_guess.sql
+++ number_guess.sql
@@ -13,8 +13,16 @@ CREATE DATABASE number_guess WITH TEMPLATE = template0;
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(22) NOT NULL UNIQUE,
-    games_played INT NOT NULL DEFAULT 0,
+    games_played INT DEFAULT 0,
     best_game INT
 );
 
 SELECT pg_catalog.setval('users_user_id_seq', 1, false);
+
+CREATE TABLE games (
+    game_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    guesses INT NOT NULL
+);
+
+SELECT pg_catalog.setval('games_game_id_seq', 1, false);
